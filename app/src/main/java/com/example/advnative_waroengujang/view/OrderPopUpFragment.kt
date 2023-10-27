ddd54786d083b9f96808810eef460e9f233e0ee3
noventaaaa <107040216+noventaaaa@users.noreply.github.com>
2023-10-27 08:26:18 +0700

    .

--- app/src/main/java/com/example/advnative_waroengujang/view/OrderPopUpFragment.kt
+++ app/src/main/java/com/example/advnative_waroengujang/view/OrderPopUpFragment.kt
@@ -1,10 +1,11 @@
-package com.example.advnative_waroengujang
+package com.example.advnative_waroengujang.view
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.advnative_waroengujang.R
 
 
 class OrderPopUpFragment : Fragment() {
